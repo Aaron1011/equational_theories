e4c63251688ae908e06e401a82a377c588d59502
Aaron Hill <aa1ronham@gmail.com>
2024-10-18 14:48:55 -0400

    Prove no overlap with some sorries

--- equational_theories/ManuallyProved/Equation1692.lean
+++ equational_theories/ManuallyProved/Equation1692.lean
@@ -25,6 +25,14 @@ theorem foo: 1 = 1 := by
     by_contra!
     simp at this
     obtain ⟨⟨k, e_k⟩, ⟨e_k_in_i, e_k_in_j⟩⟩ := this
+
+
+    have k_le_first: k ≤ 2^i := by
+      sorry
+
+    have k_le_second: k ≤ 2^j := by
+      sorry
+
     have e_k_in_i: k ≡ 2^i [MOD 2^(i + 1)] := by
       simp [s_i] at e_k_in_i
       exact e_k_in_i.2
@@ -33,105 +41,43 @@ theorem foo: 1 = 1 := by
       simp [s_i] at e_k_in_j
       exact e_k_in_j.2
 
-    --have foo := Nat.modEq_and_modEq_iff_modEq_mul sorry
+    apply (Nat.modEq_iff_dvd' k_le_first).mp at e_k_in_i
+    obtain ⟨c, hc⟩ := e_k_in_i
 
-
-    have pow_divides: 2^(i+1) ∣ 2^(j+1) := by
-      refine (Nat.pow_dvd_pow_iff_le_right ?w).mpr ?_
-      simp
-      linarith
-
-
-    let my_gcd := Nat.gcd (2^(i+1)) (2^(j+1))
-    have my_gcd_eq: my_gcd = 2^(i+1) := by
-      simp only [my_gcd]
-      exact Nat.gcd_eq_left pow_divides
-
-    have gcd_div_i: my_gcd ∣ 2^(i+1) := by
-      exact Nat.gcd_dvd_left (2^(i+1)) (2^(j+1))
-
-    have gcd_div_j: my_gcd ∣ 2^(j+1) := by
-      exact Nat.gcd_dvd_right (2 ^ (i + 1)) (2 ^ (j + 1))
-
-    obtain ⟨p, hp⟩ := gcd_div_i
-    rw [hp] at e_k_in_i
-
-    obtain ⟨q, hq⟩ := gcd_div_j
-    rw [hq] at e_k_in_j
-
-    have gcd_gt_zero: 0 < my_gcd := by
-      simp only [my_gcd]
-      apply Nat.gcd_pos_of_pos_left
-      simp
-
-    have gcd_div_coprime: Nat.Coprime (2^(i+1) / my_gcd) (2^(j+1) / my_gcd) := by
-      simp only [my_gcd]
-      apply Nat.coprime_div_gcd_div_gcd gcd_gt_zero
-
-
-    have k_eq_div_i: 2^i ≡ k [MOD 2^(i + 1) / my_gcd] := by
+    have k_i_eq_sum: k = 2^i + 2^(i + 1) * c := by
       sorry
 
-    have k_eq_div_j: 2^j ≡ k [MOD 2^(j + 1) / my_gcd] := by
+
+    have k_i_factor: k = 2^i * (1 + 2 * c) := by
+      rw [k_i_eq_sum]
+      ring
+
+    apply (Nat.modEq_iff_dvd' k_le_second).mp at e_k_in_j
+    obtain ⟨d, hd⟩ := e_k_in_j
+    have k_j_eq_sum: k = 2^j + 2^(j + 1) * d := by
       sorry
 
-    have gcd_divides: my_gcd ∣ 2^j - 2^i := by
-      sorry
+    have k_j_factor: k = 2^j * (1 + 2 * d) := by
+      rw [k_j_eq_sum]
+      ring
 
-    have exp_mono: 2^i < 2^j := by
-      apply pow_lt_pow_right
+    have two_factor_i: (2^i * (1 + 2 * c)).factorization 2 = i := by
+      rw [Nat.factorization_mul]
+      rw [Nat.Prime.factorization_pow (Nat.prime_two)]
+      simp [Nat.factorization_eq_zero_of_not_dvd]
       simp
-      assumption
-
-    have not_divide: ¬(my_gcd ∣ 2^j - 2^i) := by
-      apply Nat.not_dvd_of_pos_of_lt
       simp
-      apply exp_mono
-      rw [my_gcd_eq]
-      linarith
 
-    rw [my_gcd_eq] at gcd_divides
-    simp at gcd_divides
+    have two_factor_j: (2^j * (1 + 2 * d)).factorization 2 = j := by
+      rw [Nat.factorization_mul]
+      rw [Nat.Prime.factorization_pow (Nat.prime_two)]
+      simp [Nat.factorization_eq_zero_of_not_dvd]
+      simp
+      simp
 
-
-
-    -- simp at e_k_in_i
-    -- apply Nat.ModEq.dvd at e_k_in_i
-    -- obtain ⟨c, hc⟩ := e_k_in_i
-
-    -- simp at e_k_in_j
-    -- apply Nat.ModEq.dvd at e_k_in_j
-    -- obtain ⟨d, hd⟩ := e_k_in_j
-
-
-
-
-    -- have k_eq_i: k = 2^i - 2^(i + 1) * c := by
-    --   sorry
-
-    -- have k_eq_j: k = 2^j - 2^(j + 1) * d := by
-    --   sorry
-
-    -- have k_fac: k = 2^i * (1 - 2 * c) := by
-    --   rw [k_eq_i]
-    --   ring
-
-    -- have k_fac2: k = 2^j * (1 - 2 * d) := by
-    --   rw [k_eq_j]
-    --   ring
-
-    -- have sides_eq: 2^i * (1 - 2 * c) = 2^j * (1 - 2 * d) := by
-    --   linarith
-
-    -- have div_pow: 2^(i - j) * (1 - 2 * c) = 1 - 2 * d := by
-    --   sorry
-
-    -- have val_eq: 2^(i - j) =  (1 - 2 * d) / (1 - 2 * c) := by
-    --   sorry
-
-
-
-
-
+    rw [← k_i_factor] at two_factor_i
+    rw [← k_j_factor] at two_factor_j
+    rw [two_factor_i] at two_factor_j
+    linarith
 
   rfl

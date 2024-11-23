4ad3fa1fe14b01488f42062cb498b27c950cd30d
Aaron Hill <aa1ronham@gmail.com>
2024-11-23 10:47:44 -0500

    Find contradiction

--- equational_theories/ManuallyProved/Equation1692.lean
+++ equational_theories/ManuallyProved/Equation1692.lean
@@ -1735,10 +1735,10 @@ lemma cross_eq_same_parent {t1 t2: ReverseTree} (h_a_neq: t1.getData.a ≠ t2.ge
       rw [t1_eq_t2] at h_a_neq
       contradiction
 
-    match t1 with
+    match h_t1: t1 with
     | .root => sorry
     | .left t1_parent =>
-        match t2 with
+        match h_t2: t2 with
           | .root => sorry
           | .left t2_parent =>
               by_cases is_t1_lt: newNum t1_parent < newNum t2_parent
@@ -1753,7 +1753,26 @@ lemma cross_eq_same_parent {t1 t2: ReverseTree} (h_a_neq: t1.getData.a ≠ t2.ge
                 simp [t1_b_zero, t2_a_zero, t2_b_zero, xSeq] at fun_congr
                 rw [Finsupp.single_apply] at fun_congr
                 simp at fun_congr
-              sorry
+                linarith
+              .
+                have is_t2_le: newNum t2_parent ≤ newNum t1_parent := by
+                  linarith
+                simp [ReverseTree.getData] at h_eq
+                have fun_congr := DFunLike.congr h_eq (x := (newNum t1_parent)) rfl
+                simp at fun_congr
+                have t2_a_zero := eval_larger_a_eq_zero t2_parent (newNum t1_parent) is_t2_le
+                have t2_b_zero := eval_larger_b_eq_zero t2_parent (newNum t1_parent) is_t2_le
+                have t1_b_zero := eval_larger_b_eq_zero t1_parent (newNum t1_parent) (by simp)
+                simp [t1_b_zero, t2_a_zero, t2_b_zero, xSeq] at fun_congr
+                rw [Finsupp.single_apply, eq_comm] at fun_congr
+                simp at fun_congr
+                have parents_eq: t2_parent = t1_parent := by
+                  exact newnum_injective t2_parent t1_parent fun_congr
+                have t1_eq_t2: t1 = t2 := by
+                  rw [parents_eq.symm] at h_t1
+                  rwa [← h_t2] at h_t1
+                rw [← h_t1, ← h_t2, t1_eq_t2] at h_a_neq
+                contradiction
           | .right t2_parent =>
               have newnums_eq: newNum t1_parent = newNum t2_parent := by
                 by_contra!

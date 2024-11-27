9ec878b5d92dce2861978800fab7bfee27ff5609
Aaron Hill <aa1ronham@gmail.com>
2024-11-27 13:31:27 -0500

    Finish non-copy-paste chunk

--- equational_theories/ManuallyProved/Equation1692.lean
+++ equational_theories/ManuallyProved/Equation1692.lean
@@ -1760,14 +1760,17 @@ lemma cross_eq_same_parent {vals: XVals} {t1 t2: @ReverseTree vals} (h_a_neq: t1
                 have is_t2_le: newNum t2_parent ≤ newNum t1_parent := by
                   linarith
                 simp [ReverseTree.getData] at h_eq
-                have fun_congr := DFunLike.congr h_eq (x := (newNum t1_parent)) rfl
+                have fun_congr := DFunLike.congr h_eq (x := vals.x_to_index (newNum t1_parent)) rfl
                 simp at fun_congr
                 have t2_a_zero := eval_larger_a_eq_zero t2_parent (newNum t1_parent) is_t2_le
                 have t2_b_zero := eval_larger_b_eq_zero t2_parent (newNum t1_parent) is_t2_le
                 have t1_b_zero := eval_larger_b_eq_zero t1_parent (newNum t1_parent) (by simp)
                 simp [t1_b_zero, t2_a_zero, t2_b_zero, xSeq] at fun_congr
+                repeat rw [vals.x_to_index_eq] at fun_congr
+                simp [basis_n] at fun_congr
                 rw [Finsupp.single_apply, eq_comm] at fun_congr
                 simp at fun_congr
+                apply vals.x_to_index_inj at fun_congr
                 have parents_eq: t2_parent = t1_parent := by
                   exact newnum_injective t2_parent t1_parent fun_congr
                 have t1_eq_t2: t1 = t2 := by
@@ -1783,23 +1786,31 @@ lemma cross_eq_same_parent {vals: XVals} {t1 t2: @ReverseTree vals} (h_a_neq: t1
                   have is_t1_le: newNum t1_parent ≤ newNum t2_parent := by
                     linarith
                   simp [ReverseTree.getData] at h_eq
-                  have fun_congr := DFunLike.congr h_eq (x := (newNum t2_parent)) rfl
+                  have fun_congr := DFunLike.congr h_eq (x := vals.x_to_index (newNum t2_parent)) rfl
                   simp at fun_congr
                   have t1_b_zero := eval_larger_b_eq_zero t1_parent (newNum t2_parent) is_t1_le
                   have t2_a_zero := eval_larger_a_eq_zero t2_parent (newNum t2_parent) (by simp)
                   have t2_b_zero := eval_larger_b_eq_zero t2_parent (newNum t2_parent) (by simp)
                   simp [t1_b_zero, t2_a_zero, t2_b_zero, xSeq] at fun_congr
-                  simp [Finsupp.single_eq_of_ne this] at fun_congr
+                  repeat rw [vals.x_to_index_eq] at fun_congr
+                  simp [basis_n] at fun_congr
+                  have vals_newnum_neq: vals.x_to_index (newNum t1_parent) ≠ vals.x_to_index (newNum t2_parent) := by
+                    apply Function.Injective.ne vals.x_to_index_inj this
+                  simp [Finsupp.single_eq_of_ne vals_newnum_neq] at fun_congr
                 . have is_t2_le: newNum t2_parent ≤ newNum t1_parent := by
                     linarith
                   simp [ReverseTree.getData] at h_eq
-                  have fun_congr := DFunLike.congr h_eq (x := (newNum t1_parent)) rfl
+                  have fun_congr := DFunLike.congr h_eq (x := vals.x_to_index (newNum t1_parent)) rfl
                   simp at fun_congr
                   have t2_a_zero := eval_larger_a_eq_zero t2_parent (newNum t1_parent) is_t2_le
                   have t2_b_zero := eval_larger_b_eq_zero t2_parent (newNum t1_parent) is_t2_le
                   have t1_b_zero := eval_larger_b_eq_zero t1_parent (newNum t1_parent) (by simp)
                   simp [t1_b_zero, t2_a_zero, t2_b_zero, xSeq] at fun_congr
-                  simp [Finsupp.single_eq_of_ne this.symm] at fun_congr
+                  repeat rw [vals.x_to_index_eq] at fun_congr
+                  simp [basis_n] at fun_congr
+                  have vals_newnum_neq: vals.x_to_index (newNum t1_parent) ≠ vals.x_to_index (newNum t2_parent) := by
+                    apply Function.Injective.ne vals.x_to_index_inj this
+                  simp [Finsupp.single_eq_of_ne vals_newnum_neq.symm] at fun_congr
               have parents_eq: t1_parent = t2_parent := by
                 exact newnum_injective t1_parent t2_parent newnums_eq
               use t1_parent
